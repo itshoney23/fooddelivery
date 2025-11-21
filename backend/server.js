eb9ee5fe30911a21e4f7278b5a798c2f63fd25d1
itshoney23 <honey23022005@gmail.com>
2025-11-21 14:03:28 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ app.use("/api/order",orderRouter)
 app.get("/",(req,res)=>{
     res.send("Honey Happy To say API Working")
 })
-
+app.get("/hikaro",(req,res)=>{
+    res.send("Honey Happy To say API Working")
+})
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
